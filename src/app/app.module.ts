526fbc9b3f4d6b5e620c4b5039d135ec67c004a4
Yasith Ishara Perera <yasithishara@gmail.com>
2024-09-04 22:27:42 +0530

    added contact and about sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import {NgOptimizedImage} from "@angular/common";
+import { ContactUsComponent } from './home-page/contact-us/contact-us.component';
+import { AboutUsComponent } from './home-page/about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    ContactUsComponent,
+    AboutUsComponent
   ],
     imports: [
         BrowserModule,
